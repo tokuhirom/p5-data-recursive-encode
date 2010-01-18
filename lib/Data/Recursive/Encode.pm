6e2439bf656d535e362259e7f6b34fd037de413c
tokuhirom <tokuhirom@gmail.com>
2010-01-18 12:22:08 +0900

    - support [, CHECK] - added docs

--- lib/Data/Recursive/Encode.pm
+++ lib/Data/Recursive/Encode.pm
@@ -40,23 +40,23 @@ sub _apply {
 }
 
 sub decode {
-    my ($class, $encoding, $stuff) = @_;
-    _apply(sub { Encode::decode $encoding, $_[0] }, $stuff);
+    my ($class, $encoding, $stuff, $check) = @_;
+    _apply(sub { Encode::decode $encoding, $_[0], $check }, $stuff);
 }
 
 sub encode {
-    my ($class, $encoding, $stuff) = @_;
-    _apply(sub { Encode::encode $encoding, $_[0] }, $stuff);
+    my ($class, $encoding, $stuff, $check) = @_;
+    _apply(sub { Encode::encode $encoding, $_[0], $check }, $stuff);
 }
 
 sub decode_utf8 {
-    my ($class, @args) = @_;
-    _apply(sub { Encode::decode_utf8($_[0]) }, @args);
+    my ($class, $stuff, $check) = @_;
+    _apply(sub { Encode::decode_utf8($_[0], $check) }, $stuff);
 }
 
 sub encode_utf8 {
-    my ($class, @args) = @_;
-    _apply(sub { Encode::encode_utf8($_[0]) }, @args);
+    my ($class, $stuff) = @_;
+    _apply(sub { Encode::encode_utf8($_[0]) }, $stuff);
 }
 
 1;
@@ -66,15 +66,57 @@ __END__
 
 =head1 NAME
 
-Data::Recursive::Encode -
+Data::Recursive::Encode - Encode/Decode Values In A Structure
 
 =head1 SYNOPSIS
 
-  use Data::Recursive::Encode;
+    use Data::Recursive::Encode;
+
+    Data::Recursive::Encode->decode('euc-jp', $data);
+    Data::Recursive::Encode->encode('euc-jp', $data);
+    Data::Recursive::Encode->decode_utf8($data);
+    Data::Recursive::Encode->encode_utf8($data);
 
 =head1 DESCRIPTION
 
-Data::Recursive::Encode is
+Data::Recursive::Encode visits each node of a structure, and returns a new
+structure with each node's encoding (or similar action). If you ever wished
+to do a bulk encode/decode of the contents of a structure, then this
+module may help you.
+
+=head1 METHODS
+
+=over 4
+
+=item decode
+
+    my $ret = Data::Recursive::Encode->decode($encoding, $data, [CHECK]);
+
+Returns a structure containing nodes which are decoded from the specified
+encoding.
+
+=item encode
+
+    my $ret = Data::Recursive::Encode->encode($encoding, $data, [CHECK]);
+
+Returns a structure containing nodes which are encoded to the specified
+encoding.
+
+=item decode_utf8
+
+    my $ret = Data::Recursive::Encode->decode_utf8($data, [CHECK]);
+
+Returns a structure containing nodes which have been processed through
+decode_utf8.
+
+=item encode_utf8
+
+    my $ret = Data::Recursive::Encode->encode_utf8($data);
+
+Returns a structure containing nodes which have been processed through
+encode_utf8.
+
+=back
 
 =head1 AUTHOR
 
@@ -82,9 +124,13 @@ Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
 
 =head1 SEE ALSO
 
+This module is inspired from L<Data::Visitor::Encode>, but this module is so heavy dependencies.
+
+L<Unicode::RecursiveDowngrade> does not supports perl5's Unicode way correctly.
+
 =head1 LICENSE
 
-Copyright (C) 2009 Tokuhiro Matsuno.
+Copyright (C) 2010 Tokuhiro Matsuno All rights reserved.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
