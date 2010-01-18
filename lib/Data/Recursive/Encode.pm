8e425392aef0080461fdfc9c235c15b04bca81ed
tokuhirom <tokuhirom@gmail.com>
2010-01-18 11:06:20 +0900

    initial import

---
+++ lib/Data/Recursive/Encode.pm
@@ -0,0 +1,35 @@
+package Data::Recursive::Encode;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Data::Recursive::Encode -
+
+=head1 SYNOPSIS
+
+  use Data::Recursive::Encode;
+
+=head1 DESCRIPTION
+
+Data::Recursive::Encode is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
